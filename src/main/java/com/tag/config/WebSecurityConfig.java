b8dadfc625ba0a134d778b36578fde5036c6057d
Youngyoon-1 <duddbs774@gmail.com>
2024-04-16 18:43:58 +0900

    fix: 프로메테우스 연동 설정 관련 빈 작성

---
+++ src/main/java/com/tag/config/WebSecurityConfig.java
@@ -0,0 +1,26 @@
+package com.tag.config;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .requestMatchers("/actuator/health", "/actuator/info", "/actuator/metrics",
+                                "/actuator/prometheus")
+                        .permitAll()
+                )
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+}
