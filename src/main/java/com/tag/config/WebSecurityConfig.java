8cd73d97b8e37f2d401742b9efe596befce868ad
Youngyoon-1 <duddbs774@gmail.com>
2024-04-18 18:27:55 +0900

    update: 프로메테우스 설정 제거 및 로깅 설정 추가 및 깃허브 workflow 수정

--- src/main/java/com/tag/config/WebSecurityConfig.java
+++ src/main/java/com/tag/config/WebSecurityConfig.java
@@ -1,25 +1,25 @@
-package com.tag.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-@EnableWebSecurity
-public class WebSecurityConfig {
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((authz) -> authz
-                        .anyRequest()
-                        .permitAll()
-                ).httpBasic(HttpBasicConfigurer::disable)
-                .csrf(AbstractHttpConfigurer::disable);
-        return http.build();
-    }
-}
+//package com.tag.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+//import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class WebSecurityConfig {
+//
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests((authz) -> authz
+//                        .anyRequest()
+//                        .permitAll()
+//                ).httpBasic(HttpBasicConfigurer::disable)
+//                .csrf(AbstractHttpConfigurer::disable);
+//        return http.build();
+//    }
+//}
