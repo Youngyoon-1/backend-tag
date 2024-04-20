6d008b36f0e195c7492ea701dbc277c1a5c5ac7a
Youngyoon-1 <duddbs774@gmail.com>
2024-04-20 20:11:46 +0900

    update: application.yml

--- src/main/java/com/tag/TagApplication.java
+++ src/main/java/com/tag/TagApplication.java
@@ -1,13 +1,15 @@
 package com.tag;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication(exclude={SecurityAutoConfiguration.class})
+@Slf4j
 public class TagApplication {
-
     public static void main(String[] args) {
+        log.info("SSL Key Store Path: " + System.getenv("SSL_KEY_STORE_PATH"));
         SpringApplication.run(TagApplication.class, args);
     }
 
